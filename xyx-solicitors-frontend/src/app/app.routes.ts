a53291442ac8eb507c7cd7168c53cc11de743779
Musa Funso Oyedele <kehindet@gmail.com>
2024-11-08 14:06:25 +0100

    Clients-management section added

--- xyx-solicitors-frontend/src/app/app.routes.ts
+++ xyx-solicitors-frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 
 import { HomeComponent } from './home';
+import { ClientsComponent } from "./clients";
 import { LoginComponent, RegisterComponent } from './account';
 import { authGuard } from './_helpers';
 
@@ -8,6 +9,7 @@ const usersRoutes = () => import('./users/users.routes').then(x => x.USERS_ROUTE
 
 export const APP_ROUTES: Routes = [
     { path: '', component: HomeComponent, canActivate: [authGuard] },
+    { path: 'clients-management', component: ClientsComponent, canActivate: [authGuard] },
     { path: 'users', loadChildren: usersRoutes, canActivate: [authGuard] },
     { path: 'account/login', component: LoginComponent },
     { path: 'account/register', component: RegisterComponent },
---
+++ xyx-solicitors-frontend/src/app/clients/clients.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    templateUrl: 'clients.component.html',
+    standalone: true
+})
+export class ClientsComponent {
+
+    constructor() {
+    
+    }
+}
