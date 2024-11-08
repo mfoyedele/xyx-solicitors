2087eadc85089189db6fbfbbdee31266e169d8cf
Musa Funso Oyedele <kehindet@gmail.com>
2024-11-08 14:13:18 +0100

    cases section added

--- xyx-solicitors-frontend/src/app/app.routes.ts
+++ xyx-solicitors-frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 
 import { HomeComponent } from './home';
 import { ClientsComponent } from "./clients";
+import { CasesComponent } from "./cases";
 import { LoginComponent, RegisterComponent } from './account';
 import { authGuard } from './_helpers';
 
@@ -9,7 +10,8 @@ const usersRoutes = () => import('./users/users.routes').then(x => x.USERS_ROUTE
 
 export const APP_ROUTES: Routes = [
     { path: '', component: HomeComponent, canActivate: [authGuard] },
-    { path: 'clients-management', component: ClientsComponent, canActivate: [authGuard] },
+    { path: 'clients', component: ClientsComponent, canActivate: [authGuard] },
+    { path: 'cases', component: CasesComponent, canActivate: [authGuard] },
     { path: 'users', loadChildren: usersRoutes, canActivate: [authGuard] },
     { path: 'account/login', component: LoginComponent },
     { path: 'account/register', component: RegisterComponent },
---
+++ xyx-solicitors-frontend/src/app/cases/cases.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    templateUrl: 'cases.component.html',
+    standalone: true
+})
+export class CasesComponent {
+
+    constructor() {
+    
+    }
+}
