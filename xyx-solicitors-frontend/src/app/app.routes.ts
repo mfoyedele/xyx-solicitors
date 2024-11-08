b314096c87c8541c651665506fc59f02bc5c8a09
Musa Funso Oyedele <kehindet@gmail.com>
2024-11-08 14:28:10 +0100

    settings section added

--- xyx-solicitors-frontend/src/app/app.routes.ts
+++ xyx-solicitors-frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home';
 import { ClientsComponent } from "./clients";
 import { CasesComponent } from "./cases";
 import { ReportsComponent } from "./reports/reports.component";
+import { SettingsComponent } from "./settings";
 import { LoginComponent, RegisterComponent } from './account';
 import { authGuard } from './_helpers';
 
@@ -14,6 +15,7 @@ export const APP_ROUTES: Routes = [
     { path: 'clients', component: ClientsComponent, canActivate: [authGuard] },
     { path: 'cases', component: CasesComponent, canActivate: [authGuard] },
     { path: 'reports', component: ReportsComponent, canActivate: [authGuard] },
+    { path: 'settings', component: SettingsComponent, canActivate: [authGuard] },
     { path: 'users', loadChildren: usersRoutes, canActivate: [authGuard] },
     { path: 'account/login', component: LoginComponent },
     { path: 'account/register', component: RegisterComponent },
---
+++ xyx-solicitors-frontend/src/app/settings/settings.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    templateUrl: 'settings.component.html',
+    standalone: true
+})
+export class SettingsComponent {
+
+    constructor() {
+    
+    }
+}
