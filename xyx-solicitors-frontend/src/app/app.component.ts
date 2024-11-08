d44729f0bf33f8eb1f374059258f79090631ed7a
Musa Funso Oyedele <kehindet@gmail.com>
2024-11-08 09:41:23 +0100

    Update angular.json

--- xyx-solicitors-frontend/src/app/app.component.ts
+++ xyx-solicitors-frontend/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NgIf } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 import { AccountService } from './_services';
@@ -9,7 +14,7 @@ import { AlertComponent } from './_components/alert.component';
 @Component({
     selector: 'app-root', templateUrl: 'app.component.html',
     standalone: true,
-    imports: [NgIf, RouterOutlet, RouterLink, RouterLinkActive, AlertComponent]
+    imports: [NgIf, RouterOutlet, RouterLink, RouterLinkActive, AlertComponent, MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule]
 })
 export class AppComponent {
     user?: User | null;
